e081320e8fe7a120bdda1e691e3fa03ebc3e7917
lixin <lixin@netpulse.com>
2019-09-24 16:42:07 +0800

    ce shi

--- app/src/main/java/com/example/jesonlee/myapplicationdemo/MainActivity.java
+++ app/src/main/java/com/example/jesonlee/myapplicationdemo/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //zhe shi yi一行没什么用得东西
     }
 }
